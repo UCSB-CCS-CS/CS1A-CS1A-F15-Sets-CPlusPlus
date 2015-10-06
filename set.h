1c6d74d77919ff88352235d8cd834c138cd3cdff
Phill Conrad <pconrad@cs.ucsb.edu>
2015-10-06 11:48:33 -0700

    Red cycle for bigger sets

--- set.h
+++ set.h
@@ -10,6 +10,11 @@ class Set {
   bool isEmpty = true;
   
  public:
+  // this is constructor for stuff such as Set x({2,3,4}),
+  //  i.e. array literals
+  Set (const std::initializer_list<int>& array) { /* stub */ }
+  Set () {} ;
+  
   string toString() const;
   Set add(int newMember) const;
 };
--- setTests01.cpp
+++ setTests01.cpp
@@ -12,3 +12,8 @@ TEST(SetTests, SingletonOneToString) {
   Set y=x.add(1);
   EXPECT_EQ(string("{1}"), y.toString());
 }
+
+TEST(SetTests, BiggerSet) {
+  Set x({3,7,4,9});
+  EXPECT_EQ(string("{3,7,4,9}"), x.toString());
+}
